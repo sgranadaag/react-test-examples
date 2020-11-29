aa786354609ab1c49b63ec7c596d154cfc974c1c
sgranadaag <santiagogranadaaguirre@gmail.com>
2020-11-28 23:27:25 -0500

    app test update

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,4 +1,5 @@
 /*
+Variables globales 
   afterAll(fn, timeout)
   afterEach(fn, timeout)
   beforeAll(fn, timeout)
@@ -17,7 +18,7 @@
   test.skip.each(table)(name, fn)
   test.todo(name)
 
-
+Funciones de comparacion
   expect(value)
   expect.extend(matchers)
   expect.anything()
@@ -33,10 +34,17 @@
   expect.stringContaining(string)
   expect.stringMatching(string | regexp)
   expect.addSnapshotSerializer(serializer)
+
+Variables que se pueden utilizar con expect()
+
   .not
+    Niega cualquier funcion
   .resolves
+    Verifica si un metodo async fue resuelto
   .rejects
+    Verifica si un metodo async fue rechazado
   .toBe(value)
+    Que es igual a value
   .toHaveBeenCalled()
   .toHaveBeenCalledTimes(number)
   .toHaveBeenCalledWith(arg1, arg2, ...)
@@ -48,33 +56,52 @@
   .toHaveLastReturnedWith(value)
   .toHaveNthReturnedWith(nthCall, value)
   .toHaveLength(number)
+    Verifica si el array de entrada tiene un tamaño igual number
   .toHaveProperty(pathLlave, valor?)
+    Permite saber si el objeto que se esta comaprando tiene una propiedad definida
+    Opcionalmente se pueden definir un valor por defecto
   .toBeCloseTo(número, númeroDigitos?)
   .toBeDefined()
+    Falla solo si se esta comparando con un valor undefined
   .toBeFalsy()
-  .toBeGreaterThan(número)
+    Pasa si el valor es falso
+  .toBeGreaterThan(número) 
+    Compara el numero ingresado con el expect para ver si es mayor
   .toBeGreaterThanOrEqual(número)
+    Compara el numero ingresado con el expect para saber si es mayor o igual
   .toBeLessThan(número)
+    Menor que
   .toBeLessThanOrEqual(número)
+    Menor o igual que
   .toBeInstanceOf(Class)
   .toBeNull()
+    Pasa si el valor es nullo
   .toBeTruthy()
+    Pasa si el valor es true
   .toBeUndefined()
+    Pasa si el valor es undefined
   .toBeNaN()
+    Pasa uncamente si el valor es nan //No es un numero
   .toContain(item)
+    Verifica si existe el item en un array (No funciona para objetos)
   .toContainEqual(item)
+    Permite ver si un objeto existe dentro de un array
   .toEqual(value)
-  .toMatch(regexpOrString)
+    Valida si un valor es igual a otro como objetos
+  .toMatch(regexpOrString) 
+    Nos ayuda a decir si existen los carcteres en el string que se pasa
+    Tambien recibe expresiones regulares
   .toMatchObject(object)
+    Permite ver si un objeto contiene los keys y atributos definidos en object
   .toMatchSnapshot(propertyMatchers?, hint?)
   .toMatchInlineSnapshot(propertyMatchers?, inlineSnapshot)
   .toStrictEqual(value)
+    Funciona para comparar arrays value = []
   .toThrow(error?)
   .toThrowErrorMatchingSnapshot(hint?)
   .toThrowErrorMatchingInlineSnapshot(inlineSnapshot)
 
 */
-
 describe('Grupo de pruebas de ejemplo', () => {
 
   test('Una prueba unitaria de algo', () => {
